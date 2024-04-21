40a930282ce910f354764f38bc61bfd59e7fb7b7
mcdivine2 <146824292+mcdivine2@users.noreply.github.com>
2024-04-21 14:02:36 +0800

    removed comment

--- Online Document Request System/src/App.jsx
+++ Online Document Request System/src/App.jsx
@@ -27,8 +27,6 @@ function App() {
       </div>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
-
-        canoy member is here
       </p>
     </>
   )
